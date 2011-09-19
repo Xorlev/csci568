6281d71985d812f3d92544a44a9e1aae534bccc9
Michael Rose <elementation@gmail.com>
2011-09-19 00:30:23 -0600

    refactored inject to a l-reduce

--- project04/similarity_metrics/euclidean_distance.rb
+++ project04/similarity_metrics/euclidean_distance.rb
@@ -1,5 +1,5 @@
 module SimilarityMetrics
   def euclidean_distance(one, two)
-    [one,two].transpose.map { |pair| (pair[0]-pair[1])**2 }.inject(0) { |acc,e| e+acc }**0.5
+    [one,two].transpose.map { |pair| (pair[0]-pair[1])**2 }.reduce(:+)**0.5
   end
 end
