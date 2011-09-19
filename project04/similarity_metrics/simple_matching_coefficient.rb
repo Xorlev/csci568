e76a2beae22c59bbebff18bbe81d9eeeb866efa1
Michael Rose <elementation@gmail.com>
2011-09-18 22:30:33 -0600

    Finished implementation of SMC

--- project04/similarity_metrics/simple_matching_coefficient.rb
+++ project04/similarity_metrics/simple_matching_coefficient.rb
@@ -2,20 +2,32 @@ require 'test/unit'
 
 module SimilarityMetrics
   def simple_matching_coefficient(one, two)
-    x = [one,two].transpose.map { |pair| pair[0] == pair[1] }
-    p = x.find_all{|x| x == true}.count
-    s = x.find_all{|x| x == true}
+    tp = [one,two].transpose
+    
+    p = map_transpose(tp) { |pair| pair[0] == true && pair[0] == pair[1] }
+    q = map_transpose(tp) { |pair| pair[0] == true && pair[1] == false }
+    r = map_transpose(tp) { |pair| pair[0] == false && pair[1] == true }
+    s = map_transpose(tp) { |pair| pair[0] == false && pair[0] == pair[1] }
+    
+    t = p + q + r + s
+    
+    (p+s)/t.to_f
   end
+  
+  def map_transpose(transpose, &block)
+    transpose.map { |pair| block.call(pair) }.find_all{|x| x == true}.count
+  end
+    
 end
 
 
 class TestEuclideanDistance < Test::Unit::TestCase
   include SimilarityMetrics
   def test_same
-    assert simple_matching_coefficient([1,1,1,1], [0,0,0,0]) == 0
+    assert simple_matching_coefficient([true,true,true,true], [false,false,false,false]) == 0
   end
   
   def test_one_similar
-    assert simple_matching_coefficient([1,1,1,1], [0,1,0,0]) == 0.25
+    assert simple_matching_coefficient([true,true,true,true], [false,true,false,false]) == 0.25
   end
 end
