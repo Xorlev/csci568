f5c774ee39e7b3f6337aec1a6dfa130026b4bc30
Michael Rose <elementation@gmail.com>
2011-09-18 22:33:21 -0600

    Added extra SMC test

--- project04/similarity_metrics/simple_matching_coefficient.rb
+++ project04/similarity_metrics/simple_matching_coefficient.rb
@@ -30,4 +30,8 @@ class TestEuclideanDistance < Test::Unit::TestCase
   def test_one_similar
     assert simple_matching_coefficient([true,true,true,true], [false,true,false,false]) == 0.25
   end
+  
+  def test_five
+    assert simple_matching_coefficient([false,true,true,true,true], [false,true,false,true,false]) == 0.60
+  end
 end
