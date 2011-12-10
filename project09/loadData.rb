8c72a180a68201f65202a05b31508552f86e3c51
Michael Rose <elementation@gmail.com>
2011-12-10 00:42:59 -0700

    Establishing connection to SQLite DB, logging SQL queries, test query

--- project09/loadData.rb
+++ project09/loadData.rb
@@ -1,1 +1,15 @@
 ## Use ActiveRecord
+$LOAD_PATH << '.'
+
+require 'active_record'
+require 'logger'
+require 'models.rb'
+
+ActiveRecord::Base.establish_connection(
+	:adapter => 'sqlite3',
+	:database => 'kdd.db'
+)
+
+ActiveRecord::Base.logger = Logger.new(STDERR)
+
+puts Track.all
